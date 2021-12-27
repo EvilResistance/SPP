b6005b222157f876f1b18bb04a57a682089ec4b9
EvilResistance <71315498+EvilResistance@users.noreply.github.com>
2021-12-27 23:04:25 +0300

    SPP5

---
+++ SPPLab5/SPPLab5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+/*Реализовать консольную программу на языке C#, которая: 
+- принимает в параметре командной строки путь к сборке .NET (EXE- или DLL-файлу);
+- загружает указанную сборку в память;
+- выводит на экран полные имена всех public-типов данных этой сборки,
+упорядоченные по пространству имен (namespace) и по имени.
+*/
+
+namespace SPPLab5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly assembly = Assembly.LoadFile(Console.ReadLine());
+
+            List<Type> types = assembly.GetTypes().ToList();
+            types = types.Where(type => type.IsPublic).OrderBy(type => type.Namespace).ThenBy(type => type.Name).ToList();
+            foreach (Type publicTypes in types)
+                Console.WriteLine(publicTypes.ToString());
+            Console.ReadLine();
+        }
+    }
+}
