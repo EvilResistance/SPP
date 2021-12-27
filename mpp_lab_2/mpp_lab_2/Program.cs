ccc50c673925200c9cf1b4c01fea86c9f0c9100d
EvilResistance <71315498+EvilResistance@users.noreply.github.com>
2021-12-27 22:56:10 +0300

    SPP2

---
+++ mpp_lab_2/mpp_lab_2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace mpp_lab_2
+{
+    public delegate void TaskDelegate(string file1, string file2);
+
+    public delegate void LoggerDelegate(string logMessage);
+    static class Program
+    {
+        public static void Logger(string logMessage)
+        {
+                
+        }
+
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("введите только 2 аргумента:");
+                return;
+            }
+
+            string dir1 = args[0];
+            string dir2 = args[1];
+
+            ThreadQueue threadQueue = new ThreadQueue(3);
+            DirectoryWork directoryWork = new DirectoryWork(dir1, dir2, threadQueue, null);
+            directoryWork.CopyDir(dir1, dir2);
+            Console.WriteLine("файлов скопировано: " + DirectoryWork.filesCopyNum);
+            Console.WriteLine("подкаталогов создано: " + DirectoryWork.dirsCreateNum);
+            Console.ReadLine();
+        }
+    }
+}
