53f07d4f924ca485bd28629a613235c348ab0476
EvilResistance <71315498+EvilResistance@users.noreply.github.com>
2021-12-27 22:58:58 +0300

    SPP3

---
+++ mpp_lab_3/mpp_lab_3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+/*Создать класс на языке C#, который: 
+- называется Mutex и реализует двоичный семафор
+с помощью атомарной операции Interlocked.CompareExchange. 
+- обеспечивает блокировку и разблокировку двоичного
+семафора с помощью public-методов Lock и Unlock.
+*/
+namespace mpp_lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            Mutex mutex = new Mutex();
+            for (int i = 0; i < N; i++)  
+            {
+                new Thread(() =>
+                {
+                    mutex.Lock();
+                    Console.WriteLine("current Thread id: " + Thread.CurrentThread.ManagedThreadId + " lock Thread");
+                    Thread.Sleep(400);
+                    Console.WriteLine("current Thread id: " + Thread.CurrentThread.ManagedThreadId + " unlock Thread");
+                    mutex.Unlock();
+                }
+                ).Start();
+            }
+            Console.ReadKey();
+        }
+    }
+}
