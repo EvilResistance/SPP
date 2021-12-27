1d917ebff2beb8d4a6d1e7ffb73ed6e6e9f267bf
EvilResistance <71315498+EvilResistance@users.noreply.github.com>
2021-12-27 22:54:19 +0300

    SPP1

---
+++ SPP1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using System.Threading;
+
+namespace SPP1
+{
+    class Program
+    {
+        static int stack = 0;
+        static int x = 0;
+        static int y = 1;
+        static object locker = new object();
+        static void counter()
+        {
+            lock (locker)
+            {
+                x = 1;
+                for(int i = 1; i <= stack; i++)
+                {
+                    Console.WriteLine("Поток #{0}: {1}", y, x);
+                    x++;
+                    Thread.Sleep(100);
+                }
+                y++;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество потоков:");
+            String line = Console.ReadLine();
+            Char[] temp = line.ToCharArray();
+            Regex regex = new Regex("[1-9]");
+            line = "";
+            for (int i = 0; i < temp.Length; i++)
+            {
+                MatchCollection matches = regex.Matches(temp[i].ToString());
+                if(matches.Count > 0)
+                {
+                    line += temp[i];
+                }
+            }
+            int count = Int32.Parse(line);
+            stack = count;
+            TaskQueue taskQueue = new TaskQueue(count);
+            for(int i = 0; i < count; i++)
+            {
+                taskQueue.EnqueueTask(counter);
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ SPP1/TaskQueue.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Concurrent;
+
+namespace SPP1
+{
+    public delegate void TaskDelegate();
+    class TaskQueue
+    {
+        static object locker = new object();
+        private BlockingCollection<TaskDelegate> QueueFunc = new BlockingCollection<TaskDelegate>(new ConcurrentQueue<TaskDelegate>()); 
+        
+        public TaskQueue(int count)
+        {
+            for(int i = 0; i < count; i++)
+            {
+                Thread thread = new Thread(Write) { IsBackground = true }; ;
+                thread.Start();
+            }
+        }
+
+        public void Write()
+        {
+            lock (locker)
+            {
+                while (true)
+                {
+                    var task = QueueFunc.Take();
+                    try
+                    {
+                        task();
+                    }
+                    catch(ThreadStateException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    catch (ThreadAbortException ex)
+                    {
+                        Thread.ResetAbort();
+                    }
+                    catch(Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                }
+            }
+        }
+
+        public void EnqueueTask(TaskDelegate task)
+        {
+            this.QueueFunc.Add(task);
+        }
+    }
+}
