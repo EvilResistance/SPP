ddda98bb378253159c4dd60afa9837cbd3998281
EvilResistance <71315498+EvilResistance@users.noreply.github.com>
2021-12-27 23:00:31 +0300

    SPP4

---
+++ mpp_lab_4/mpp_lab_4/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+//Создать класс на языке C#, который: 
+//- называется OSHandle и обеспечивает автоматическое или принудительное освобождение заданного дескриптора операционной системы;
+//- содержит свойство Handle, позволяющее установить и получить дескриптор операционной системы; 
+//- реализует метод Finalize для автоматического освобождения дескриптора;
+//- реализует интерфейс IDisposable для принудительного освобождения дескриптора; 
+
+namespace lab
+{
+    class OsHandle : IDisposable
+    {
+        [DllImport("Kernel32.dll")]
+        private static extern bool CloseHandle(IntPtr handle);
+
+        private bool _disposed = false;
+        public IntPtr Handle { get; set; }
+
+        public OsHandle()
+        {
+            Handle = IntPtr.Zero;
+        }
+
+        ~OsHandle()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);//Сообщает среде CLR, что она не должна вызывать метод завершения для указанного объекта.
+        }
+
+        public void Close()
+        {
+            Dispose();
+        }
+
+        protected virtual void Dispose(bool _disposed)
+        {
+                if (!_disposed && Handle != IntPtr.Zero)
+                {
+                    CloseHandle(Handle);
+                    Handle = IntPtr.Zero;
+                }
+                _disposed = true;
+        }
+    }
+    //dispose для неуправляемых ресурсов как дескриптор, finalize для управляемых
+    //финализ вызывается при создании обьекта
+
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            OsHandle os = new OsHandle();
+            os.Dispose();
+        }
+    }
+}
